e7e23bd758061a4b7f106078cd3b7ec2b1a9d446
timothy <timothy9369@gmail.com>
2021-10-20 16:50:33 +0300

    implementing update cart items

--- store/actions/index.js
+++ store/actions/index.js
@@ -7,23 +7,54 @@ export const actionTypes = {
 };
 
 export const addToCart = (product, cart) => {
-  
   if (product.inStock > 0) {
     const cartProducts = [...cart.products];
-
     const idx = cartProducts.findIndex((prod) => prod._id === product._id);
 
     if (idx === -1) {
-      
+      const sum = product.price * 1;
+      const newCart = {
+        ...cart,
+        products: [...cartProducts, { ...product, quantity: 1, sum }],
+        total: cart.total + sum,
+      };
+
+      return { type: actionTypes.ADD_TO_CART, payload: newCart };
+    } else {
+      const sum = cartProducts[idx].sum + cartProducts[idx].price;
+      const cartTotal = cart.total + cartProducts[idx].price;
+      cartProducts[idx] = {
+        ...cartProducts[idx],
+        quantity: cartProducts[idx].quantity + 1,
+        sum,
+      };
+
+      return {
+        type: actionTypes.ADD_TO_CART,
+        payload: { ...cart, products: cartProducts, total: cartTotal },
+      };
     }
   }
-  
 };
 
 export const reduceCartItem = (product, cart) => {
-  
+  const cartProducts = [...cart.products];
+  const idx = cartProducts.findIndex((prod) => prod._id === product._id);
+
+  cartProducts[idx] = {
+    ...cartProducts[idx],
+    quantity: cartProducts[idx].quantity - 1,
+    sum: cartProducts[idx].sum - cartProducts[idx].price,
+  };
+
+  return {
+    type: actionTypes.REDUCE_FROM_CART,
+    payload: {
+      ...cart,
+      products: cartProducts,
+      total: cart.total - cartProducts[idx].price,
+    },
+  };
 };
 
-export const deleteCartItem = (product, cart) => {
-  
-};
+export const deleteCartItem = (product, cart) => {};
