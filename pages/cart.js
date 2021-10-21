67a62b6eba7117f75daedf27e999bec9fe045b7f
timothy <timothy9369@gmail.com>
2021-10-21 07:55:58 +0300

    deleting cart

--- pages/cart.js
+++ pages/cart.js
@@ -28,30 +28,43 @@ const Cart = () => {
           <table className="mt-4 table-auto w-full lg:w-2/3">
             <tbody className="">
               {cart.products.map((prod) => (
-                <CartItem key={prod._id} product={prod} cart={cart} dispatch={dispatch} />
+                <CartItem
+                  key={prod._id}
+                  product={prod}
+                  cart={cart}
+                  dispatch={dispatch}
+                />
               ))}
             </tbody>
           </table>
           <div className="mt-4 lg:w-1/4 space-y-4 max-w-xl mx-auto">
             <h2 className="text-lg text-center font-semibold">Shipping</h2>
             <div className="flex flex-col">
-              <label htmlFor="address" className="text-sm">Address</label>
+              <label htmlFor="address" className="text-sm">
+                Address
+              </label>
               <input
                 type="text"
                 className="border py-1 bg-gray-50 focus:outline-none focus:bg-white"
               />
             </div>
             <div className="flex flex-col">
-              <label htmlFor="mobile" className="text-sm">Mobile</label>
+              <label htmlFor="mobile" className="text-sm">
+                Mobile
+              </label>
               <input
                 type="text"
                 className="border py-1 bg-gray-50 focus:outline-none focus:bg-white"
               />
             </div>
             <Link href="/">
-              <a className="mt-2 block text-light font-bold">Continue Shopping</a>
+              <a className="mt-2 block text-light font-bold">
+                Continue Shopping
+              </a>
             </Link>
-            <div className="text-lg font-semibold">Total: <span className="ml-1 text-red">${cart.total}</span></div>
+            <div className="text-lg font-semibold">
+              Total: <span className="ml-1 text-red">${cart.total}</span>
+            </div>
             <button className="w-full py-1 bg-light text-white font-bold">
               Proceed to checkout
             </button>
